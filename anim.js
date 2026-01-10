3ca49ffaf5623f00642d2f9f9c66b23ad9fd155b
Nikhilkale2 <nikhilkaledev@gmail.com>
2026-01-10 18:41:52 +0530

    practice responsive navigation bar

--- anim.js
+++ anim.js
@@ -8,7 +8,6 @@ mm.add("(min-width: 1400px)", () => {
       start: "top 60%",
       end: "bottom 40%",
       scrub: true,
-      markers: true,
     },
   });
   let tl2 = gsap.timeline({
@@ -18,7 +17,6 @@ mm.add("(min-width: 1400px)", () => {
       start: "top 80%",
       end: "bottom 60%",
       scrub: true,
-      markers: true,
     },
   });
   let navbar = gsap.timeline();
@@ -106,8 +104,142 @@ mm.add("(min-width: 1400px)", () => {
     "d"
   );
   tl2.from(".ad-section .ad-box", {
-    x: -400,
+    x: 400,
     opacity: 0,
-    duration: 1.5,
+    duration: 1.2,
   });
 });
+
+mm.add("(max-width:768px)", () => {
+  let navbar = gsap.timeline();
+  let hero = gsap.timeline();
+  let about = gsap.timeline();
+  navbar.from(".section1 .logo h1, .links a, .icons i", {
+    x: -200,
+    opacity: 0,
+    duration: 1.3,
+    stagger: 0.3,
+  });
+  hero.from(
+    ".hero-img img",
+    {
+      y: -800,
+      duration: 1.2,
+      opacity: 0,
+    },
+    "hero"
+  );
+  hero.from(
+    ".hero-info h1, p, button",
+    {
+      y: 300,
+      duration: 1.1,
+      opacity: 0,
+      stagger: 0.4,
+    },
+    "hero"
+  );
+
+  about.from(".item1", {
+    y: -450,
+    duration: 1.2,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".item1",
+      scroller: "body",
+      start: "top 40%",
+      end: "top 15%",
+      scrub: true,
+    },
+  });
+  about.from(".item2", {
+    y: -450,
+    duration: 1.2,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".item2",
+      scroller: "body",
+      start: "top 40%",
+      end: "top 15%",
+      scrub: true,
+    },
+  });
+  about.from(".item3", {
+    y: -450,
+    duration: 1.3,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".item3",
+      scroller: "body",
+      start: "top 40%",
+      end: "top 15%",
+      scrub: true,
+    },
+  });
+  about.from(".item4", {
+    y: -450,
+    duration: 1.3,
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".item4",
+      scroller: "body",
+      start: "top 40%",
+      end: "top 15%",
+      scrub: true,
+    },
+  });
+  about.from(".item5", {
+    y: -450,
+    opacity: 0,
+    duration: 0.9,
+    scrollTrigger: {
+      trigger: ".item5",
+      scroller: "body",
+      start: "top 40%",
+      end: "top 15%",
+      scrub: true,
+    },
+  });
+  about.from(".item6", {
+    y: -450,
+    opacity: 0,
+    duration: 0.9,
+    scrollTrigger: {
+      trigger: ".item6",
+      scroller: "body",
+      start: "top 40%",
+      end: "top 15%",
+      scrub: true,
+    },
+  });
+
+  let ads = gsap.timeline();
+  gsap.from(".ad-box img, .ad-theory h1,.ad-theory p", {
+    y: -450,
+    opacity: 0,
+    duration: 0.9,
+    stagger: 0.5,
+    scrollTrigger: {
+      trigger: ".ad-section",
+      scroller: "body",
+      start: "top 40%",
+      end: "top 15%",
+      scrub: true,
+    },
+  });
+});
+
+// let ads = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".ad-box",
+//     scroller: "body",
+//     start: "top 60%",
+//     end: "top 30%",
+//     scrub: true,
+//   },
+// });
+// ads.from(".ad-box img", {
+//   y: -450,
+//   opacity: 0,
+//   duration: 0.9,
+// });
